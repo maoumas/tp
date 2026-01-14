097e281e0b3f60bc72ece5c5cf865b9c5bc885dd
Mauricio Umana <69874300+maoumas@users.noreply.github.com>
2026-01-13 21:45:08 -0500

    Add files via upload

---
+++ orar/img/userpalette.js
@@ -0,0 +1,27 @@
+        // --- PALETA V25: ESCALERA DE CRISTAL (Espectro Completo) ---
+        const userPalette = [
+            "#4a0e0e", // 1. Rojo Caoba (Tierra)
+            "#851515", // 2. Rojo Rubí
+            "#c0392b", // 3. Rojo Fuego
+            "#d35400", // 4. Naranja Quemado
+            "#e67e22", // 5. Naranja Vida
+            "#f39c12", // 6. Dorado Atardecer
+            "#f1c40f", // 7. Oro Puro
+            "#f7dc6f", // 8. Luz Solar
+            "#abebc6", // 9. Verde Brote
+            "#2ecc71", // 10. Esmeralda
+            "#1abc9c", // 11. Turquesa
+            "#16a085", // 12. Mar Profundo
+            "#3498db", // 13. Azul Cielo
+            "#2980b9", // 14. Azul Océano
+            "#5dade2", // 15. Azul Claro
+            "#8e44ad", // 16. Púrpura Real
+            "#9b59b6", // 17. Amatista
+            "#af7ac5", // 18. Orquídea
+            "#bb8fce", // 19. Lavanda
+            "#d2b4de", // 20. Lila
+            "#e8daef", // 21. Niebla
+            "#f4ecf7", // 22. Luz Rosada
+            "#fbfcfc", // 23. Luz Blanca
+            "#ffffff"  // 24. Totalidad
+        ];
